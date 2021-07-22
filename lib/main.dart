93bdb0af7f418daae32c6c744207a055423d6214
Sensor2049 <87636706+Sensor2049@users.noreply.github.com>
2021-07-22 15:39:12 +0500

    wtf

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'MyApp.dart';
 
-void main() {
+Future<void> main() async {
+  await Hive.initFlutter();
   runApp(new MaterialApp( debugShowCheckedModeBanner: false, home: MyApp()));
 }
