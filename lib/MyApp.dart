2c2d11ddd8feb8d7a9d046d211ccc5a9e20db570
Ogryzkov <lexus301@mail.ru>
2021-07-22 15:17:09 +0500

    commit 1(aleksey)

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -18,16 +18,22 @@ class MyApp extends StatefulWidget {
 class MyAppState extends State<MyApp> {
   MapController _controller = MapController();
   late Position _current;
+  double _zoom = 7;//масштаба
+  bool isLocationServiceEnabled = false;
+
+
 
   @override
   void initState() {
     super.initState();
     initPosition();
+    tryGeo();
   }
 
   void initPosition() async {
     _current = (await Geolocator.getLastKnownPosition())!;
     _updateLocation();
+    isLocationServiceEnabled  = await Geolocator.isLocationServiceEnabled();//метод показывающий включена ли служба определения геопозиции
   }
 
   Future<void> _updateCurrent() async {
@@ -41,8 +47,37 @@ class MyAppState extends State<MyApp> {
   void _updateLocation() {
     _updateCurrent();
     LatLng center = new LatLng(_current.latitude, _current.longitude);
-    _controller.move(center, 14);
+    _controller.move(center, _zoom);//_zoom позволит соблюдать масштаб при переопределении геопозиции
   }
+
+  void _zoomPlus() {
+    LatLng center = new LatLng(_current.latitude, _current.longitude);
+    _zoom += 0.5;
+    _controller.move(center, _zoom);
+  }
+
+  void _zoomMinus() {
+    LatLng center = new LatLng(_current.latitude, _current.longitude);
+    _zoom -= 0.5;
+    _controller.move(center, _zoom);
+  }
+
+  Widget tryGeo() {
+    if (isLocationServiceEnabled)
+      {
+       return Text("Yes",
+        textDirection: TextDirection.ltr,       // текст слева направо
+        textAlign: TextAlign.center,            // выравнивание по центру
+        style: TextStyle(color: Colors.green,   // зеленый цвет текста
+        fontSize: 26,                       // высота шрифта 26
+        backgroundColor: Colors.black87));
+      }     // черный цвет фона текста
+    else
+       return Text("No");
+
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -58,25 +93,62 @@ class MyAppState extends State<MyApp> {
           mapController: _controller,
           layers: [
             TileLayerOptions(
-              urlTemplate: URL_OSM_TEMPLATE,
-              subdomains: ['a', 'b', 'c']
+              urlTemplate: URL_SPUTNIK_TEMPLATE,
+             //subdomains: ['a', 'b', 'c']
             )
           ],
         ),
         Align(
           child: Padding(
               padding: EdgeInsets.all(25),
-              child : FloatingActionButton(
+              child : /*FloatingActionButton(
                 child: Icon(
                   Icons.gps_fixed_sharp,
                   color: Colors.white,
                   semanticLabel: 'Update a GPS location',
                 ),
                 onPressed: _updateLocation,
+              )*/
+              Column(children:[
+                FloatingActionButton(
+                child: Icon(
+                  Icons.gps_fixed_sharp,
+                  color: Colors.white,
+                  semanticLabel: 'Update a GPS location',
+                ),
+                onPressed: _updateLocation,),
+
+                FloatingActionButton(
+                  child: Icon(
+                    Icons.add,
+                    color: Colors.greenAccent,
+                    semanticLabel: 'Zoom plus',
+                  ),
+                  onPressed: _zoomPlus,),
+
+                FloatingActionButton(
+                  child: Icon(
+                    Icons.horizontal_rule,
+                    color: Colors.white,
+                    semanticLabel: 'Zoom minus',
+                  ),
+                  onPressed: _zoomMinus,),
+              ]
               )
+
+
+
           ),
           alignment: Alignment.bottomRight,
+        ),
+
+
+        Align(
+            child: tryGeo(),
+          alignment:Alignment.center ,
         )
+
+
       ],
     );
   }
